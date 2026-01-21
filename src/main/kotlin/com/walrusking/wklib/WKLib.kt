14e57e03a25b1e5454de9dd72c85ba8b281adc61
Walrusking <walruskinggaming@gmail.com>
2026-01-20 22:40:49 -0500

    removes empty space after imports

--- src/main/kotlin/com/walrusking/wklib/WKLib.kt
+++ src/main/kotlin/com/walrusking/wklib/WKLib.kt
@@ -6,7 +6,6 @@ import com.walrusking.wklib.components.Components
 import com.walrusking.wklib.events.Events
 import com.walrusking.wklib.plugins.WKPlugin
 
-
 class WKLib(init: JavaPluginInit) : WKPlugin("WKLib", init) {
 	override fun setup() {
 		LOGGER.info("Initializing WKLib...")
