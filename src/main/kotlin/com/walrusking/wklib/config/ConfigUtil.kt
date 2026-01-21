fc31ee12000fb98d651164ac318ef9f42c14f611
Walrusking <walruskinggaming@gmail.com>
2026-01-21 01:53:21 -0500

    adds config support

---
+++ src/main/kotlin/com/walrusking/wklib/config/ConfigUtil.kt
@@ -0,0 +1,30 @@
+﻿package com.walrusking.wklib.config
+
+import com.hypixel.hytale.server.core.util.Config
+import com.walrusking.wklib.logging.WKLogger
+import com.walrusking.wklib.plugins.WKPlugin
+import java.util.Locale.getDefault
+
+class ConfigUtil {
+	companion object {
+		fun getConfigName(configName: String): String {
+			return configName.replace(" ", "").lowercase(getDefault())
+		}
+
+		fun loadConfig(plugin: WKPlugin, config: Config<*>) {
+			val pluginName = plugin.pluginName
+
+			val configName = getConfigName(pluginName)
+			val dataDirectory = plugin.dataDirectory
+			val pluginConfig = dataDirectory.resolve("$configName.json").toFile()
+
+			if (!pluginConfig.exists()) {
+				config.save()
+				WKLogger("WKLib:Configs").info("Created new config $pluginName")
+			} else {
+				config.load()
+				WKLogger("WKLib:Configs").info("Loaded existing config $pluginName")
+			}
+		}
+	}
+}
--- src/main/kotlin/com/walrusking/wklib/plugins/WKPlugin.kt
+++ src/main/kotlin/com/walrusking/wklib/plugins/WKPlugin.kt
@@ -2,15 +2,26 @@
 
 import com.hypixel.hytale.server.core.plugin.JavaPlugin
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit
+import com.hypixel.hytale.server.core.util.Config
+import com.walrusking.wklib.config.ConfigUtil
 import com.walrusking.wklib.logging.WKLogger
+import com.walrusking.wklib.utilities.CodecUtil
 
 
-open class WKPlugin(name: String, init: JavaPluginInit) : JavaPlugin(init) {
+open class WKPlugin(val pluginName: String, init: JavaPluginInit) : JavaPlugin(init) {
 	companion object {
 		lateinit var LOGGER: WKLogger
 	}
 
 	init {
-		LOGGER = WKLogger(name)
+		LOGGER = WKLogger(pluginName)
+	}
+
+	protected inline fun <reified T> withConfig(): Config<T> {
+		return this.withConfig(pluginName, CodecUtil.buildConfigCodec(T::class.java))
+	}
+
+	protected inline fun <reified T> withConfig(configName: String): Config<T> {
+		return this.withConfig(ConfigUtil.getConfigName(configName), CodecUtil.buildConfigCodec(T::class.java))
 	}
 }
