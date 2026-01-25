0c33f75eac15e9e754cb1069b60ab1af779c8f41
Walrusking <walruskinggaming@gmail.com>
2026-01-24 21:47:35 -0500

    adds WKBlockGlobalDelayedEntitySystem.kt

---
+++ src/main/kotlin/com/walrusking/wklib/components/WKBlockGlobalDelayedEntitySystem.kt
@@ -0,0 +1,26 @@
+﻿package com.walrusking.wklib.components
+
+import com.hypixel.hytale.component.ArchetypeChunk
+import com.hypixel.hytale.component.CommandBuffer
+import com.hypixel.hytale.component.Store
+import com.hypixel.hytale.component.system.tick.DelayedEntitySystem
+import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore
+
+abstract class WKBlockGlobalDelayedEntitySystem(
+	intervalSec: Float
+) :
+	DelayedEntitySystem<ChunkStore>(intervalSec) {
+
+	override fun tick(
+		p0: Float,
+		p1: Int,
+		p2: ArchetypeChunk<ChunkStore>,
+		p3: Store<ChunkStore>,
+		p4: CommandBuffer<ChunkStore>
+	) {
+		val data = BlockGlobalEntityTickingData(p0, p1, p2, p3, p4)
+		onTick(data)
+	}
+
+	abstract fun onTick(data: BlockGlobalEntityTickingData)
+}
