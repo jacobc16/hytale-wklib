e2b264eea93ad93511946784d3a9a266b4204a3d
Walrusking <walruskinggaming@gmail.com>
2026-01-24 21:47:44 -0500

    adds WKBlockDelayedEntitySystem

---
+++ src/main/kotlin/com/walrusking/wklib/components/WKBlockDelayedEntitySystem.kt
@@ -0,0 +1,32 @@
+﻿package com.walrusking.wklib.components
+
+import com.hypixel.hytale.component.*
+import com.hypixel.hytale.component.query.Query
+import com.hypixel.hytale.component.system.tick.DelayedEntitySystem
+import com.hypixel.hytale.server.core.universe.world.storage.ChunkStore
+
+abstract class WKBlockDelayedEntitySystem<T : Component<ChunkStore>>(
+	val componentType: ComponentType<ChunkStore, T>,
+	intervalSec: Float
+) :
+	DelayedEntitySystem<ChunkStore>(intervalSec) {
+
+	override fun tick(
+		p0: Float,
+		p1: Int,
+		p2: ArchetypeChunk<ChunkStore>,
+		p3: Store<ChunkStore>,
+		p4: CommandBuffer<ChunkStore>
+	) {
+		val component: T = p2.getComponent(p1, componentType) ?: return
+
+		val data = BlockEntityTickingData(p0, p1, p2, p3, p4, component)
+		onTick(data)
+	}
+
+	abstract fun onTick(data: BlockEntityTickingData<T>)
+
+	override fun getQuery(): Query<ChunkStore>? {
+		return Query.and(componentType)
+	}
+}
