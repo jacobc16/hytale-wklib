8f22ff3a79d8fa4087920bff3b1ad4d07e52d8bd
Walrusking <walruskinggaming@gmail.com>
2026-01-25 20:27:33 -0500

    adds WKNPCDeathSystem

---
+++ src/main/kotlin/com/walrusking/wklib/systems/WKNPCDeathSystem.kt
@@ -0,0 +1,26 @@
+﻿package com.walrusking.wklib.systems
+
+import com.hypixel.hytale.component.query.Query
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore
+import com.hypixel.hytale.server.npc.entities.NPCEntity
+
+/**
+ * An abstract system that handles NPC death events by extending WKDeathSystem.
+ */
+abstract class WKNPCDeathSystem<T : WKOnDeathEvent> : WKDeathSystem() {
+	/**
+	 * Method to be overridden by subclasses to implement custom NPC death event handling logic.
+	 *
+	 * @param event The WKOnDeathEvent containing information about the NPC death event and context.
+	 */
+	override fun onDeath(event: WKOnDeathEvent) {}
+
+	/**
+	 * Overrides the default query to filter for entities that have the NPCEntity component.
+	 *
+	 * @return The Query for entities with the NPCEntity component.
+	 */
+	override fun getQuery(): Query<EntityStore> {
+		return Query.and(NPCEntity.getComponentType())
+	}
+}
